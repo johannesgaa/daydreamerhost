b398cffc43b5eb2b5edb7b6eec8abb6d2e0b4ec9
JG <jg.ingaa@gmail.com>
2021-01-03 16:19:33 +0100

    init

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 # The binary to build (just the basename).
-MODULE := blueprint
+MODULE := daydreamerhost
 
 # Where to push the docker image.
-REGISTRY ?= docker.pkg.github.com/martinheinz/python-project-blueprint
+REGISTRY ?= docker.pkg.github.com/johannesgaa/daydreamerhost
 
 IMAGE := $(REGISTRY)/$(MODULE)
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,9 @@
-from .context import blueprint
+from .context import daydreamerhost
 
 
 def test_app(capsys, example_fixture):
     # pylint: disable=W0612,W0613
-    blueprint.Blueprint.run()
+    daydreamerhost.Blueprint.run()
     captured = capsys.readouterr()
 
     assert "Hello World..." in captured.out
